936122df8b9cd7c5afc82ecefa066defd9fc30a4
Jon Herron <jon.herron@yahoo.com>
2024-01-06 22:22:14 -0500

    WIP

--- qbe_backend/src/lib.rs
+++ qbe_backend/src/lib.rs
@@ -79,8 +79,21 @@ fn lower_decls(decls: &Vec<m::Decl>) -> Vec<Decl> {
         .collect()
 }
 
-fn lower_args(_args: &m::FuncArgs) -> FuncArgs {
-    todo!()
+fn lower_args(args: &m::FuncArgs) -> FuncArgs {
+    fn lower(args: &Vec<m::FuncArg>) -> Vec<FuncArg> {
+        args.iter().map(|a| lower_arg(a)).collect()
+    }
+
+    match args {
+        m::FuncArgs::Fixed(args) => FuncArgs::Fixed(lower(args)),
+        m::FuncArgs::Variadic(first, rest) => FuncArgs::Variadic(lower_arg(first), lower(rest)),
+    }
+}
+
+fn lower_arg(arg: &m::FuncArg) -> FuncArg {
+    match arg {
+        m::FuncArg::Named(name, r#type) => FuncArg::Named(name.to_string(), lower_type(r#type)),
+    }
 }
 
 fn lower_stmts(_stmts: &Vec<m::Stmt>) -> Vec<Stmt> {
