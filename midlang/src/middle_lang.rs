d0f051c5aa0995920f909be3d6411b701652c680
jbirddog <100367399+jbirddog@users.noreply.github.com>
2024-01-12 10:48:20 -0500

    Port the type checker (#19)

--- midlang/src/middle_lang.rs
+++ midlang/src/middle_lang.rs
@@ -8,11 +8,13 @@ pub enum Decl {
     FuncDecl(String, Visibility, Type, FuncArgs, Vec<Stmt>),
 }
 
+#[derive(PartialEq)]
 pub enum FuncArgs {
     Fixed(Vec<FuncArg>),
     Variadic(FuncArg, Vec<FuncArg>),
 }
 
+#[derive(PartialEq)]
 pub enum FuncArg {
     Named(String, Type),
 }
@@ -28,12 +30,54 @@ pub enum Expr {
     FuncCall(String, Type, Vec<Expr>),
 }
 
+#[derive(PartialEq)]
 pub enum Visibility {
     Public,
     Private,
 }
 
+#[derive(PartialEq)]
 pub enum Type {
     Int32,
     Str,
+}
+
+impl Expr {
+    pub fn r#type(&self) -> &Type {
+        match self {
+            Self::ConstInt32(_) => &Type::Int32,
+            Self::ConstStr(_) => &Type::Str,
+            Self::FuncCall(_, r#type, _) => r#type,
+        }
+    }
+}
+
+impl FuncArg {
+    pub fn name(&self) -> &str {
+        match self {
+            Self::Named(name, _) => name,
+        }
+    }
+
+    pub fn r#type(&self) -> &Type {
+        match self {
+            Self::Named(_, r#type) => r#type,
+        }
+    }
+}
+
+impl FuncArgs {
+    pub fn is_empty(&self) -> bool {
+        match self {
+            Self::Fixed(v) => v.is_empty(),
+            Self::Variadic(_, _) => false,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        match self {
+            Self::Fixed(v) => v.len(),
+            Self::Variadic(_, v) => v.len() + 1,
+        }
+    }
 }
