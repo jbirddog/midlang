461e2ab908b52ea7e7ea5c10aae302e75e71c684
Jon Herron <jon.herron@yahoo.com>
2024-01-14 18:28:11 -0500

    Allow specifying output file name, library search paths and libraries to link

--- mlc/src/main.rs
+++ mlc/src/main.rs
@@ -10,12 +10,25 @@ struct Args {
     build_dir: String,
     #[arg(short, long)]
     ninja: String,
+    #[arg(short, long = "library")]
+    libraries: Option<Vec<String>>,
+    #[arg(short = 'L', long = "library-path")]
+    library_paths: Option<Vec<String>>,
+    #[arg(short, long)]
+    output: Option<String>,
 }
+
+const DEFAULT_OUTPUT: &str = "a.out";
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
     let frontend = json_frontend::new(&args.json_file);
-    let backend = qbe_backend::new();
+
+    let libraries = args.libraries.unwrap_or_else(Vec::new);
+    let library_paths = args.library_paths.unwrap_or_else(Vec::new);
+    let output = args.output.unwrap_or_else(|| DEFAULT_OUTPUT.to_string());
+    let backend = qbe_backend::new(&libraries, &library_paths, &output);
+
     let compiler = compiler::new(&frontend, &backend, &args.build_dir, &args.ninja);
 
     compiler.compile()
